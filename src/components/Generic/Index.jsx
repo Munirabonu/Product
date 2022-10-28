9f0d9a7d263b717ba5a7a9fd7719c7ba5e6fc8f5
Munirabonu <muniratillaxojayeva@gmail.com>
2022-10-28 21:43:57 +0500

    first

---
+++ src/components/Generic/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+export default function Generic() {
+  return (
+    <div>
+      <h1>Generic</h1>
+    </div>
+  )
+}
---
+++ src/components/Navbar/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.css'
+export default function Navbar() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  )
+}
