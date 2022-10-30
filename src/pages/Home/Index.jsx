bf71ffff1ae7b5cc43dbe6e1c5f4da6239d40288
Munirabonu <muniratillaxojayeva@gmail.com>
2022-10-30 05:01:11 +0500

    for_you

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ForYou from '../../components/For_you/Index';
 import Navbar from '../../components/Navbar/Index';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <ForYou />
     </div>
   )
 }
