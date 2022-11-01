36b1498a74c2fee37ccd3a06b4b9cf444649475a
Soliha <solyatursuntasheva@gmail.com>
2022-11-01 21:40:44 +0500

    finish navbar already

--- src/components/Navbar/Index.js
+++ src/components/Navbar/Index.js
@@ -10,6 +10,7 @@ export default function Navbar() {
   };
   return (
     // starts Navbar
+    // bismillah
     <div className="navbar container">
       <div className="logo-container">
         <a href="">
