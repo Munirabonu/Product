aed86df810d179a5aa8e1c659d2081030792b224
Munirabonu <muniratillaxojayeva@gmail.com>
2022-11-05 08:51:44 +0500

    price

---
+++ src/components/Price/Index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css";
+
+import {price} from "../../utils/price";
+export default function Price() {
+  return (
+    <div className="price_wrap container">
+        <div className="price_cards">
+          {price?.map((item) => {
+            return (
+              <div className="price_card_item">
+                <p className="card_title">{item.title}</p>
+                <p className="card_desc">{item?.desc}</p>
+                <div className="per_wrap">
+                  <p className="per_price">{item?.price}</p>
+                  <p className="per_status">$</p>
+                  <p className="per_type">Per/month</p>
+                </div>
+                <p className="card_operators">{item?.operators}</p>
+                <p className="card_add_one">{item?.add_one}</p>
+                <p className="card_add_one">{item?.add_two}</p>
+                <button className="card_btn">Order Now</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+  );
+}
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+
 import ForYou from '../../components/For_you/Index';
 import Navbar from '../../components/Navbar/Index';
 import Hero from "../../components/Hero/Index"
 import Join from "../../components/Join/Index"
+import Price from '../../components/Price/Index';
+
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero/>
       <ForYou />
+      <Price />
       <Join/>
     </div>
   )
