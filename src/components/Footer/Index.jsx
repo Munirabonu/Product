f254f908d7958a54ea6530c50f90e79a74165d5c
Munirabonu <muniratillaxojayeva@gmail.com>
2022-11-16 22:01:20 +0500

    done

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -6,7 +6,7 @@ import "./style.css";
 export default function Footer() {
   return (
     <div className="footer container">
-      <div className="flex">
+      <div className="layout">
         <div className="logo_wrap">
           <div className="logo-container">
             <Link to="/">
