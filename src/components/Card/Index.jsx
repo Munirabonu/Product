4b0635d337e9cb3dcf89b62c5868204f1b859eab
Munirabonu <muniratillaxojayeva@gmail.com>
2022-11-21 08:52:33 +0500

    done

--- src/components/Card/Index.jsx
+++ src/components/Card/Index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 export default function Card({id,img,by,date,text}) {
   return (
     <div className='card_wrap' key={id}>
-        <img src={img} alt='photo' />
+        <img src={img} alt='card' />
         <div className="text_wrap">
           <h1 className='font-size_1626'>By <span>{by}</span> |  {date}</h1>
           <p className='font-size_2233'>{text}</p>
